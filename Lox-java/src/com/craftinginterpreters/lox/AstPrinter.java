109379e0fc1a85c2d4fc8aa671557d462262efbe
Joy-Chenwi <coderj652@gmail.com>
2025-05-27 22:40:10 +0400

    the Abstract Syntax Tree addiotion

--- Lox-java/src/com/craftinginterpreters/lox/AstPrinter.java
+++ Lox-java/src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,6 +1,19 @@
 package com.craftinginterpreters.lox;
 
 class AstPrinter implements Expr.Visitor<String> {
+
+    public static void main (String[] args) {
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123))
+            new Token(TokenType.STAR, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -30,7 +43,7 @@ class AstPrinter implements Expr.Visitor<String> {
             builder.append(" ");
             builder.append(expr.accept(this));
         }
-        builder.append("j");
+        builder.append(")");
 
         return builder.toString();
     }
